b3961b68fa07e2e302e031c1cb25d17a8a950572
LeandroGiovani <leandrogiovani@outlook.com>
2025-02-27 18:09:53 -0300

    servidor

---
+++ backEnd/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
